626c1df52576dc3ac2730695244f99f8d6e516a6
gonzalo.guasch <gonzaloguasch@outlook.com>
2020-05-26 14:36:28 -0300

    facturacion WIP

--- src/components/Factura.js
+++ src/components/Factura.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import NavBar from '../components/BarraBusqueda'
 import '../css/FinalizarCompra.css'
+import Boton from '../components/WppButton'
 
 export default class Factura extends React.Component{
     constructor(props) {
@@ -30,11 +31,33 @@ export default class Factura extends React.Component{
                         <div><input type="text" className="input"/></div>
                     </div>   
                 </div>
+                <div className="nombre-apellido-container">
+                    <div className="box-tittle"></div>
+
+                </div>
                     <div className="box-email">
-                        <div className="email">Email </div> 
-                        <div><input type="text" className="input"/></div>
+                        <div>
+                            <div className="email">Email </div> 
+                            <div><input type="text" className="input"/></div>
+                        </div>
+                        <div className="inputs">
+                            <div className="email">Codigo Postal</div>
+                            <div><input type="text" className="input"/></div>
+                        </div>
                     </div>
+                    <div className="box-email">
+                            <div className="direccion">
+                                <div className="email">Direccion </div> 
+                                <div><input type="text" className="input"/></div>
+                            </div>
+                            <div className="aclaracion">
+                            <div className="email">Aclaracion</div>
+                            <div><input type="text" className="input" placeholder="Piso, Dpto"/></div>
+                        </div>
+                    </div>
+                   
                     <input type="button" value="FINALIZAR COMPRA" className="finalizar-compra-button"/>
+                <Boton></Boton>
                 </div>
                 
         );
--- src/components/WppButton.js
+++ src/components/WppButton.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import '../css/BotonWpp.css'
+import wpp from '../images/whatsapp.svg'
 export default function BotonWpp(){
 
     return (
         <div>   
             <a href="https://api.whatsapp.com/send?phone=5491139327763&text=Hola!%20Vengo%20de%20la%20Web%20de%20Home%20Store%20Market" 
             target="_blank"
-            className="wpp-boton">¿Cómo podemos ayudarte?</a>       
+            className="wpp-boton"><img src={wpp} className="wpp-pop-up"/>¿Cómo podemos ayudarte?</a>       
         </div>
     );
 }
