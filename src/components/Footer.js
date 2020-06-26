6275b79227d920e100ccdc0c26ed3bac457207bf
gonzalo.guasch <gonzaloguasch@outlook.com>
2020-06-26 11:31:31 -0300

    footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,38 @@
-import React from 'react'
-import '../css/footer.css'
+import React, { Fragment } from 'react'
 import email from '../images/email.svg'
 import cell from '../images/phone.svg'
 import facebook from '../images/faibu.png'
 import instagram from '../images/ig.png'
+import '../css/footer.css'
+
 
 export default function Footer(){
+
     return(
-        <div className="footer-container">
-            <div className="red-container">
-                <p>CONTACTO</p>
-                <div><img src={cell} className="icon-container" alt="cellphone"></img>CEL/TEL</div>
-                <div><img src={email} className="icon-container" alt="email" ></img> EMAIL</div>   
+        <Fragment>
+            <div className="footer-container">
+                <div className="contacto-footer-container">
+                    <p className="contacto-tittle">Contacto</p>
+                        <div className="instagram-contacto">
+                            <img src={cell} className="icono-container" alt="cellphone"></img>5555-5555
+                        </div>
+                        <div className="facebook-contacto">
+                            <img src={email} className="icono-container" alt="email" ></img> soytumarket@gmail.com
+                        </div>
+                </div>
+                <div className="redes-footer-container">
+                    <p className="redes-title">Redes</p>
+                        <div className="i-logo">
+                            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                                <img src={instagram} height="20px" alt="instagram logo" /></a>Seguinos en instagram!</div>
+                        <div>
+                            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                                <img src={facebook} height="20px" alt="facebook logo"/></a> Seguinos en facebook!</div>
+                        </div>
+                <div className="direccion-footer-container">
+                    <p className="redes-title">Direccion</p>
+                </div>
             </div>
-            <div className="red-container">
-                <p>REDES</p>
-                <div><a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-                    <img src={instagram} height="20px" alt="instagram logo"/></a> Seguinos en instagram!</div>
-                <div><a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-                    <img src={facebook} height="20px" alt="facebook logo"/></a> Seguinos en facebook!</div>
-            </div>
-            <div>
-            <p>DIRECCION</p>
-            </div>
-        </div>
+        </Fragment>
     )
 }
---
+++ src/components/FooterOld.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../css/footer.css'
+
+
+export default function Footer(){
+    return(
+        <div className="footer-container">
+            <div className="red-container">
+                <p>CONTACTO</p>
+                <div></div>
+                <div> EMAIL</div>   
+            </div>
+            <div className="red-container">
+                <p>REDES</p>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+            <p>DIRECCION</p>
+            </div>
+        </div>
+    )
+}
